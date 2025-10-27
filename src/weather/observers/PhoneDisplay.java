a9003eb0a49c84df2eeb45f00e3d20991d98eff5
Altusha4 <240782@astanait.edu.kz>
2025-10-27 22:33:16 +0600

    add PhoneDisplay observer

--- src/weather/observers/PhoneDisplay.java
+++ src/weather/observers/PhoneDisplay.java
@@ -1,4 +1,22 @@
 package weather.observers;
 
-public class PhoneDisplay {
+import weather.core.Observer;
+import weather.core.WeatherData;
+
+public class PhoneDisplay implements Observer {
+    String owner;
+
+    public PhoneDisplay(String owner) {
+        this.owner = owner;
+    }
+    @Override
+    public void update(WeatherData data) {
+        System.out.println("[Phone:" + owner + "] "
+                + data.getCity() + " | "
+                + data.getTemperature() + "°C, "
+                + data.getHumidity() + "%, "
+                + data.getPressure() + " hPa, "
+                + data.getWindSpeed() + " m/s | "
+                + data.getTimestamp());
+    }
 }
