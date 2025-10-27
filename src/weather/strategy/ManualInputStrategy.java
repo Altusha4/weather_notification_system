75e11c3665ea1b34d2bd1fa7ee6ebe56c20fa13b
Altusha4 <240782@astanait.edu.kz>
2025-10-27 22:29:47 +0600

    add ManualInputStrategy for user-entered weather data

--- src/weather/strategy/ManualInputStrategy.java
+++ src/weather/strategy/ManualInputStrategy.java
@@ -1,4 +1,30 @@
 package weather.strategy;
 
-public class ManualInputStrategy {
+import weather.core.WeatherData;
+import java.time.Instant;
+import java.util.Scanner;
+
+public class ManualInputStrategy implements UpdateStrategy {
+    Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public WeatherData fetchData() {
+        System.out.println("Enter city: ");
+        String city = scanner.nextLine();
+
+        System.out.println("Enter temperature (°C): ");
+        double temp = scanner.nextDouble();
+
+        System.out.println("Enter humidity (%): ");
+        double hum = scanner.nextDouble();
+
+        System.out.println("Enter pressure (hPa): ");
+        double press = scanner.nextDouble();
+
+        System.out.println("Enter wind speed (m/s): ");
+        double wind = scanner.nextDouble();
+        scanner.nextLine();
+
+        return new WeatherData(city, temp, hum, press, wind, Instant.now());
+    }
 }
