26e7f9344156ae06fc40e551603d0e52b7d78760
Altusha4 <240782@astanait.edu.kz>
2025-10-27 22:23:05 +0600

    implement WeatherStation with observer and strategy integration

--- src/weather/core/WeatherStation.java
+++ src/weather/core/WeatherStation.java
@@ -1,4 +1,49 @@
 package weather.core;
+import weather.strategy.UpdateStrategy;
+import java.util.ArrayList;
+import java.util.List;
 
-public class WeatherStation {
+public class WeatherStation implements Subject {
+    String name;
+    UpdateStrategy strategy;
+    List<Observer> observers = new ArrayList<>();
+    WeatherData lastData;
+
+    public WeatherStation(String name, UpdateStrategy strategy) {
+        this.name = name;
+        this.strategy = strategy;
+    }
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+    @Override
+    public void notifyObservers(WeatherData data) {
+        for (Observer observer : observers) {
+            observer.update(data);
+        }
+    }
+
+    public void setStrategy(UpdateStrategy strategy) {
+        this.strategy = strategy;
+        System.out.println("Strategy set");
+    }
+    public void updateWeather() {
+        if (strategy == null) {
+            System.out.println("No update strategy set!");
+        }
+        WeatherData newData = strategy.fetchData();
+        lastData = newData;
+        notifyObservers(newData);
+    }
+    public WeatherData getLastData() {
+        return lastData;
+    }
+    public String getName() {
+        return name;
+    }
 }
