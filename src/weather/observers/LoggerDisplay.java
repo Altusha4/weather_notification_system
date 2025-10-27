1cf2f64ec57cc84ac294c23df0d6d5c4d315a15b
Altusha4 <240782@astanait.edu.kz>
2025-10-27 22:35:41 +0600

    add LoggerDisplay observer

--- src/weather/observers/LoggerDisplay.java
+++ src/weather/observers/LoggerDisplay.java
@@ -1,4 +1,11 @@
 package weather.observers;
 
-public class LoggerDisplay {
+import weather.core.Observer;
+import weather.core.WeatherData;
+
+public class LoggerDisplay implements Observer {
+    @Override
+    public void update(WeatherData data) {
+        System.out.println("[Logger] Weather record saved: " + data);
+    }
 }
